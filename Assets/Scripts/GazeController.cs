0401bacce8e4d367c12e2bd7aab3569458d345ee
gnda <kevin.nandacoumar@gmail.com>
2019-10-23 16:58:06 +0200

    First commit

---
+++ Assets/Scripts/GazeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class GazeController : MonoBehaviour
+{
+    public Sprite gazePointRed;
+    public Sprite gazePointGreen;
+    public Sprite gazePointBlue;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<GazePlotterCustom>().PointSprite = gazePointRed;
+        } else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GetComponent<GazePlotterCustom>().PointSprite = gazePointGreen;
+        } else if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<GazePlotterCustom>().PointSprite = gazePointBlue;
+        }
+    }
+}
