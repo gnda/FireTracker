8032286d0c3653d865ff4d50708df4922d3a9d5b
Riverbor <quentin-du-9@hotmail.fr>
2019-10-24 12:56:24 +0200

    Select Drone + anime arrow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,13 @@
-﻿using Tobii.Gaming;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tobii.Gaming;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] GameObject m_selectDrone;
+    [SerializeField] List<GameObject> m_placeholderDrones;
+
     private int _last;
     private GazePoint _lastGazePoint = GazePoint.Invalid;
 
@@ -14,8 +19,30 @@ public class PlayerController : MonoBehaviour
         if (gazePoint.IsRecent()
             && gazePoint.Timestamp > (_lastGazePoint.Timestamp + float.Epsilon))
         {
-            Debug.Log((int) gazePoint.Screen.x);
-            Debug.Log((int) gazePoint.Screen.y);
+            Debug.Log((int)gazePoint.Screen.x);
+            Debug.Log((int)gazePoint.Screen.y);
         }
+
+        if (m_selectDrone != null && m_placeholderDrones.Any())
+        {
+
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Vector3 placeholderDrone1 = m_placeholderDrones.First().transform.position;
+                m_selectDrone.transform.position = placeholderDrone1;
+            }
+            else if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Vector3 placeholderDrone2 = m_placeholderDrones.Skip(1).First().transform.position;
+                m_selectDrone.transform.position = placeholderDrone2;
+            }
+            else if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Vector3 placeholderDrone3 = m_placeholderDrones.Last().transform.position;
+                m_selectDrone.transform.position = placeholderDrone3;
+            }
+        }
+
+
     }
 }
