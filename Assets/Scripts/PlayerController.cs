8e886671825831fec447766d1bf14fa5b9d42dd3
gnda <kevin.nandacoumar@gmail.com>
2019-10-24 11:05:40 +0200

    Work on PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using Tobii.Gaming;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int _last;
+    private GazePoint _lastGazePoint = GazePoint.Invalid;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GazePoint gazePoint = TobiiAPI.GetGazePoint();
+
+        if (gazePoint.IsRecent()
+            && gazePoint.Timestamp > (_lastGazePoint.Timestamp + float.Epsilon))
+        {
+            Debug.Log((int) gazePoint.Screen.x);
+            Debug.Log((int) gazePoint.Screen.y);
+        }
+    }
+}
